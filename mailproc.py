c940bea0984451904d8621abe06d1ac85e8c4167
Arik Baratz <github@arik.baratz.org>
2019-08-30 14:29:30 +0200

    added more realistic mail processing

--- mailproc.py
+++ mailproc.py
@@ -1,9 +1,18 @@
 import sys
-import email
+from email import message_from_binary_file
+from bloxs import Bloxs
 
 
 def main():
-    open('/home/mailuser/test.eml', 'rb').write(sys.stdin.read())
+    bloxs = Bloxs()
+    message = message_from_binary_file(sys.stdin.buffer)
+    open('/home/mailuser/test.eml', 'wb').write(message.as_bytes())
+    for part in message.walk():
+        if part.get_content_type() == 'application/pdf':
+            open('/home/mailuser/test.pdf', 'wb').write(
+                part.get_payload(decode=True))
+            bloxs.create_draft_purchase_invoice('/home/mailuser/test.pdf')
+
 
 if __name__ == '__main__':
     main()
